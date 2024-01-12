98623f517e573cd1b47e570e159e21f751ac43fb
anshawasthi88 <anshawasthi88@gmail.com>
2024-01-12 19:14:53 +0530

    adding some css to cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -14,18 +14,19 @@ const CartItem = ({item,itemIndex}) => {
 
   }
   return (
-  <div>
+  <div  className="flex flex-col items-center justify-between hover:scale-110 transition duration-300 ease-in gap-3 p-4 mt-10 ml-5 rounded bg-white shadow-md w-96">
        <div>
-        <div>
-          <img src={item.image} alt="" />
+        <div className="flex">
+          <img width={60}  src={item.image} alt="" />
+          <h1>{item.title}</h1>
         </div>
         <div>
-          <h1>{item.title}</h1>
+          
           <h1>{item.description}</h1>
           <div>
-            <p>{item.price}</p>
-            <div onClick={removeFromCart}>
-              <FcDeleteDatabase/>
+            <p>${item.price}</p>
+            <div onClick={removeFromCart} className="w-full h-full">
+              <FcDeleteDatabase width={100} height={100}/>
             </div>
           </div>
         </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -15,38 +15,39 @@ const Cart = () => {
   },[cart])
 
   return (
-      <div className="flex">
-        {
+      <div className="flex mt-10 ">
+          {
           cart.length > 0 ? 
           (
-            <div>
-              {
-                cart.map((item,index) => (
-                  <CartItem key = {item.id} item={item} itemIndex={index}/>
-                ))
-              }
-              <div>
-                <div>
-                  <div>Your Cart</div>
-                  <div>Summary</div>
-                  <p>Total Items : {cart.length} </p>
+              <div className="space-y-4 flex justify-center space-x-10 mx-auto">
+                <div >
+                  {
+                    cart.map((item,index) => (
+                      <CartItem key = {item.id} item={item} itemIndex={index}/>
+                    ))
+                  }
                 </div>
                 <div>
-                  <p>Total amount : ${totalAmount}</p>
-                  <button>CheckOut</button>
+                    <div className="font-bold">Your Cart</div>
+                    <div className="font-bold">Summary</div>
+                    <p className="font-semibold">Total Items : {cart.length} </p>
+                    <div className="space-y-2">
+                      <p className="font-semibold">Total amount : ${totalAmount.toFixed(2)}</p>
+                      <button className="bg-green-500 text-white px-4 py-2">CheckOut</button>
+                    </div>
                 </div>
               </div>
-          </div>
-      )
-          :(<div>
-              <h1>Cart Empty</h1>
+
+          )
+          :(<div className="text-center flex-col space-y-4 flex items-center justify-center w-full h-[500px]">
+              <h1 className="text-2xl font-bold">Cart Empty</h1>
               <Link to={"/"}>
-              <button>Shop Now</button>
+              <button className="bg-blue-500 text-white px-4 py-2">Shop Now</button>
               </Link>
               
           </div>)
         }
-  </div>
+      </div>
   )
 };
 
